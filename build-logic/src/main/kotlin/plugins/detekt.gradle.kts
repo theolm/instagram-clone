efdb2b088853de9a8f9b3c2f53aab093a99f841c
Theodoro Mota <theolm.mota@gmail.com>
2024-02-18 18:07:04 -0300

    auto fixes

--- build-logic/src/main/kotlin/plugins/detekt.gradle.kts
+++ build-logic/src/main/kotlin/plugins/detekt.gradle.kts
@@ -27,7 +27,7 @@ private val scriptsFiles = "**/*.kts"
 detekt {
     buildUponDefaultConfig = true
     parallel = true
-    autoCorrect = false
+    autoCorrect = true
     ignoreFailures = false
     config.setFrom(configFile)
     baseline = file(baselineFile)
