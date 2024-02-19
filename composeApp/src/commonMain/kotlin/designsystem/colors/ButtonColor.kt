c98eb13a19ef2f8df4a329b7fd11bbf5f9b4497a
Ricardo Martins <ricardomartinsqa@gmail.com>
2024-02-19 15:40:20 -0300

    feature: adds color enum resources

---
+++ composeApp/src/commonMain/kotlin/designsystem/colors/ButtonColor.kt
@@ -0,0 +1,8 @@
+package designsystem.colors
+
+import androidx.compose.ui.graphics.Color
+
+enum class ButtonColor(val color: Color) {
+    BLUE_50A(Color(0x803797EF)),
+    BLUE_100A(Color(0xFF3797EF))
+}
