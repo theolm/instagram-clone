7f5bede3a865c6bd831f768fbe609eafdd2dd68e
Theodoro Mota <theolm.mota@gmail.com>
2024-02-18 22:39:39 -0300

    removing application plugin

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -1,30 +1,112 @@
+import config.Config
+import org.gradle.accessors.dm.LibrariesForLibs
+import org.jetbrains.compose.ExperimentalComposeLibrary
+
 plugins {
-    id("application-setup")
+    id("com.android.application")
+    id("org.jetbrains.kotlin.multiplatform")
+    id("org.jetbrains.compose")
     id("plugins.detekt")
 }
 
+android {
+    namespace = Config.applicationId
+    compileSdk = Config.compileSdk
+
+    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+    sourceSets["main"].res.srcDirs("src/androidMain/res")
+    sourceSets["main"].resources.srcDirs("src/commonMain/resources")
+
+    defaultConfig {
+        applicationId = Config.applicationId
+        minSdk = Config.minSdk
+        targetSdk = Config.targetSdk
+        versionCode = Config.versionCode
+        versionName = Config.versionName
+    }
+
+    compileOptions {
+        sourceCompatibility = Config.javaVersion
+        targetCompatibility = Config.javaVersion
+    }
+
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+        }
+    }
+
+    dependencies {
+        debugImplementation(libs.compose.ui.tooling)
+    }
+
+}
+
 kotlin {
+    androidTarget {
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = Config.javaVersion.toString()
+            }
+        }
+    }
+
+    jvm("desktop")
+
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach { iosTarget ->
+        iosTarget.binaries.framework {
+            baseName = "ComposeApp"
+            isStatic = true
+        }
+    }
+
+    val libs = the<LibrariesForLibs>()
     sourceSets {
         val desktopMain by getting
-        
+
         androidMain.dependencies {
+            implementation(libs.koin.core)
+            implementation(libs.koin.android)
             implementation(libs.compose.ui.tooling.preview)
             implementation(libs.androidx.activity.compose)
         }
 
         commonMain.dependencies {
             implementation(projects.designSystem)
+            implementation(projects.resources)
+            implementation(compose.runtime)
+            implementation(compose.foundation)
+            implementation(compose.material)
+            implementation(compose.ui)
+            @OptIn(ExperimentalComposeLibrary::class)
+            implementation(compose.components.resources)
+            implementation(libs.koin.core)
+            implementation(libs.koin.test)
+            implementation(libs.koin.compose)
             implementation(libs.voyager.navigator)
             implementation(libs.voyager.screenModel)
             implementation(libs.voyager.koin)
-            implementation(projects.resources)
             implementation(libs.moko.compose.resources)
         }
-    }
-}
 
-android {
-    dependencies {
-        debugImplementation(libs.compose.ui.tooling)
+        desktopMain.dependencies {
+            implementation(compose.desktop.currentOs)
+            implementation(libs.kotlin.coroutines.swing)
+        }
+
+        iosMain.dependencies {
+            //Workaround to fix koin on ios
+            implementation(libs.stately.common)
+        }
     }
 }
